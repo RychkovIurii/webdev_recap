f565578503c290ef1c8abd6ecf1276fde1114762
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-10-09 15:17:57 +0300

    Added button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,6 +29,28 @@ const App = () => {
       .catch((error) => console.log("Error fetching data.", error));
   };
 
+  const renderItem = () => {
+    const newItem = todoList;
+    return newItem.map((item) => (
+      <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
+          {item.title}
+      </li>
+    ));
+  }
+
+  const handleAdd = () => {
+    const newTodo = { title: "New Task text" };
+    fetch("/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTodo),
+    })
+      .then(() => getList())
+      .catch((error) => console.log("Error adding task.", error));
+  }
+
   return (
 
     <main className="container">
@@ -37,7 +59,11 @@ const App = () => {
         <div className="col-md-6 col-sm-10 mx-auto p-0">
           <div className="card p-3">
             <div className="mb-4">
+              <ul className="list-group list-group-flush border-top-0">
+                {renderItem()}
+              </ul>
             </div>
+            <button className="btn btn-primary" onClick={handleAdd}>Add Task</button>
           </div>
         </div>
       </div>
